71f51b13d2934e6f91152fb5ce35a2a3cfe2e50f
Elisha Garcia <garciaelisha93@yahoo.com>
2026-04-27 14:43:00 -0500

    initial spring boot setup with hello endpoint

---
+++ src/test/java/com/egg/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
